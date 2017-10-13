7a95dd922b8346e1e014328201f5d830da417561
Jason Williamson <nexus6ca@gmail.com>
2017-10-13 12:24:22 -0700

    Hamburgers

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,11 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108085151-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
     gtag('js', new Date());
 
     gtag('config', 'UA-108085151-1');
@@ -25,12 +29,16 @@
 <link rel="shortcut icon" href="{{{ asset('/images/favicon.ico') }}}">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=oh5ws399rzhqudty6ewt08gbx60u3eyz8q29xcmarl0o31gi"></script>
 <script>tinymce.init({
-        selector:'textarea',
+        selector: 'textarea',
         plugins: "hr code anchor autoresize table image",
 
-});
+    });
 </script>
 <script type="text/javascript" src="{{ URL::asset('/js/main.js') }}"></script>
 <link rel="stylesheet" href="css/blueimp-gallery.min.css">
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,36 +1,44 @@
 <div class="header">
-    <div class="header_text"><h1 style="font-size:3vw">The Nanaimo Open Chess Tournament</h1></div>
+    <div class="header_text"><h1 style="font-size: 4vw;">The Nanaimo Open Chess Tournament</h1></div>
 </div>
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
+<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+    <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand" href="/nanaimo_chessclub">Nanaimo Chess Club</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
         </div>
-        <ul class="nav navbar-nav">
-            <?php if ($active == null) {
-                $active="home";
-            } ?>
-            <li @if($active=='home')class="active"@endif><a href="/">Home</a></li>
-            <li @if($active=='next_tournament')class="active"@endif><a href="/next_tournament">Current Tournament</a></li>
-            <li @if($active=='previous_tournament')class="active"@endif><a href="/previous_tournament">Past Tournaments</a></li>
-            <li @if($active=='gallery')class="active"@endif><a href="/gallery">Gallery</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            @if(Auth::guest())
-                <li><a href="/register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            @else
-                <li><a href="/profile/display"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-                <li><a href="/logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-            @endif
-        </ul>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <?php if ($active == null) {
+                    $active="home";
+                } ?>
+                <li @if($active=='home')class="active"@endif><a href="/">Home</a></li>
+                <li @if($active=='next_tournament')class="active"@endif><a href="/next_tournament">Current Tournament</a></li>
+                <li @if($active=='previous_tournament')class="active"@endif><a href="/previous_tournament">Past Tournaments</a></li>
+                <li @if($active=='gallery')class="active"@endif><a href="/gallery">Gallery</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                @if(Auth::guest())
+                    <li><a href="/register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                @else
+                    <li><a href="/profile/display"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+                    <li><a href="/logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                @endif
+            </ul>
+        </div>
     </div>
 </nav>
 @if((Auth::check()) && Auth::user()->isAdmin)
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/backend/home">Site Controls</a>
+            <a class="navbar-brand" href="/backend/home">Site Control</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="/backend/pages/browse">Manage Pages</a></li>
